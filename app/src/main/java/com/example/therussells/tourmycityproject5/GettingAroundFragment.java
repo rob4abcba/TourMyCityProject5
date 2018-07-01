510e216af3035cf8e616cb12e7e580e0d067617f
TheRussells <dain.russell@gmail.com>
2018-07-01 00:02:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/therussells/tourmycityproject5/GettingAroundFragment.java
@@ -0,0 +1,29 @@
+package com.example.therussells.tourmycityproject5;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GettingAroundFragment extends Fragment {
+
+
+    public GettingAroundFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_getting_around, container, false);
+    }
+
+}
